7e58082e440d1d12f5752d98f9f2bd2ce00b6e7d
miPera <migperalta10@gmail.com>
2021-01-18 22:00:50 -0600

    Simplify error message to show only failing validation fields and their corresponding messages only

--- backend/src/main/java/com/example/kanbantool/web/ProjectController.java
+++ backend/src/main/java/com/example/kanbantool/web/ProjectController.java
@@ -5,10 +5,16 @@ import com.example.kanbantool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +24,17 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
-        Project project1 = projectService.saveOrUpdateProject(project);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 }
