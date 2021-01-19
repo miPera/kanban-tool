d84bb5e9ab9fefe6a19a78f7e672fb042c2c3285
miPera <migperalta10@gmail.com>
2021-01-18 21:22:00 -0600

    Add functionality to create and persist a new project.

--- backend/src/main/java/com/example/kanbantool/services/ProjectService.java
+++ backend/src/main/java/com/example/kanbantool/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.example.kanbantool.services;
 
+import com.example.kanbantool.domain.Project;
 import com.example.kanbantool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,7 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
 }
