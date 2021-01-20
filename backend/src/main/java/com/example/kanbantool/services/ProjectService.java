e761fb8d69173a943246d50a57e3cf607b8a16db
miPera <migperalta10@gmail.com>
2021-01-19 21:57:49 -0600

    Add error handling for case where user tries to save project with id that already exists. Set a project id on save to database.

--- backend/src/main/java/com/example/kanbantool/services/ProjectService.java
+++ backend/src/main/java/com/example/kanbantool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.kanbantool.services;
 
 import com.example.kanbantool.domain.Project;
+import com.example.kanbantool.exceptions.ProjectIdException;
 import com.example.kanbantool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
