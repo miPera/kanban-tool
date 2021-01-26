2cb7aabd7bcd020d3675ff2aab4ab1745a9069b3
miPera <migperalta10@gmail.com>
2021-01-25 21:03:29 -0600

    Add project details and modification buttons to project items. Add dashboard content, sign up and login buttons.

--- front-end-client/src/components/Project/ProjectItem.js
+++ front-end-client/src/components/Project/ProjectItem.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import {
+  faEdit,
+  faFlagCheckered,
+  faMinusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class ProjectItem extends Component {
   render() {
-    return <h1>Project Item</h1>;
+    return (
+      <div className="container">
+        <div className="card card-body bg-light mb-3">
+          <div className="row">
+            <div className="col-2">
+              <span className="mx-auto">REACT</span>
+            </div>
+            <div className="col-lg-6 col-md-4 col-8">
+              <h3>Spring / React Project</h3>
+              <p>Project to create a Kanban Board with Spring Boot and React</p>
+            </div>
+            <div className="col-md-4 d-none d-lg-block">
+              <ul className="list-group">
+                <a href="#">
+                  <li className="list-group-item board">
+                    <FontAwesomeIcon icon={faFlagCheckered} /> Project Board
+                  </li>
+                </a>
+                <a href="#">
+                  <li className="list-group-item update">
+                    <span className="fa fa-edit">
+                      <FontAwesomeIcon icon={faEdit} /> UpdateProject Info
+                    </span>
+                  </li>
+                </a>
+                <a href="">
+                  <li className="list-group-item delete">
+                    <span className="fa fa-minus-circle">
+                      <FontAwesomeIcon icon={faMinusCircle} /> Delete Project
+                    </span>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
