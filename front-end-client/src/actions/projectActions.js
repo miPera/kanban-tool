9476edbcb7f06b365b17b52603aa8cfbf8eb10f4
miPera <migperalta10@gmail.com>
2021-02-10 21:20:40 -0600

    Add redux flow to retrieve project being update on Project Update page.

--- front-end-client/src/actions/projectActions.js
+++ front-end-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
@@ -22,6 +22,14 @@ export const getProjects = () => async (dispatch) => {
   const response = await axios.get("http://localhost:8080/api/project/all");
   dispatch({
     type: GET_PROJECTS,
+    payload: response.data,
+  });
+};
+
+export const getProject = (id, history) => async (dispatch) => {
+  const response = await axios.get(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
     payload: response.data,
   });
 };
